dc9234926c45deea7b868d8536d2dad71c423717
Florian Vallée (ey3ball) <florian@vallee.xyz>
2022-12-05 22:35:26 +0100

    lint: cargo fmt

--- aoclib/src/day04.rs
+++ aoclib/src/day04.rs
@@ -10,7 +10,7 @@ trait ExtendedRange {
 }
 
 impl ExtendedRange for Section {
-    fn contained(&self, other: &Self)  -> bool {
+    fn contained(&self, other: &Self) -> bool {
         other.contains(&self.start()) && other.contains(&self.end())
     }
     fn overlaps(&self, other: &Self) -> bool {
@@ -21,26 +21,30 @@ impl ExtendedRange for Section {
 impl Day04 {
     pub fn parse(input: &str) -> Day04 {
         Day04 {
-            sections: input.lines()
-             .map(|l| {
-                let parsed: Vec<usize> = l.split(",").flat_map(
-                     |e| e.split("-").map(|num| num.parse().unwrap())
-                ).collect();
-                ((parsed[0]..=parsed[1]),(parsed[2]..=parsed[3]))
-             })
-             .collect()
+            sections: input
+                .lines()
+                .map(|l| {
+                    let parsed: Vec<usize> = l
+                        .split(",")
+                        .flat_map(|e| e.split("-").map(|num| num.parse().unwrap()))
+                        .collect();
+                    ((parsed[0]..=parsed[1]), (parsed[2]..=parsed[3]))
+                })
+                .collect(),
         }
     }
 
     pub fn part1(&self) -> usize {
-        self.sections.iter()
-                .filter(|(s1,s2)| s1.contained(s2) || s2.contained(s1))
-                .count()
+        self.sections
+            .iter()
+            .filter(|(s1, s2)| s1.contained(s2) || s2.contained(s1))
+            .count()
     }
 
     pub fn part2(&self) -> usize {
-        self.sections.iter()
-                .filter(|(s1,s2)| s1.overlaps(s2))
-                .count()
+        self.sections
+            .iter()
+            .filter(|(s1, s2)| s1.overlaps(s2))
+            .count()
     }
 }

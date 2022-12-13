a61a14944012185553d6fea8cb83bcb6d8a276ed
Florian Vallée (ey3ball) <florian@vallee.xyz>
2022-12-13 22:49:07 +0100

    day13: solve part1

--- aoclib/src/day13.rs
+++ aoclib/src/day13.rs
@@ -7,7 +7,52 @@ use nom::{
     sequence::delimited,
     IResult,
 };
-type Input = String;
+
+#[derive(PartialEq, Debug, Clone)]
+enum Packet {
+    Integer(usize),
+    List(Vec<Packet>),
+}
+
+type Input = Vec<(Packet, Packet)>;
+
+impl Packet {
+    fn parse(input: &str) -> IResult<&str, Packet> {
+        Ok(alt((
+            delimited(
+                char('['),
+                map(separated_list0(char(','), Self::parse), |p| Packet::List(p)),
+                char(']'),
+            ),
+            map(digit1, |out: &str| Packet::Integer(out.parse().unwrap())),
+        ))(input)?)
+    }
+
+    fn compare(&self, other: &Self) -> Option<bool> {
+        match (self, other) {
+            (Packet::Integer(v1), Packet::Integer(v2)) => if v1 == v2 { None } else { Some(v1 < v2) }
+            (Packet::Integer(v1), Packet::List(_)) => Packet::List(vec![Packet::Integer(*v1)]).compare(other),
+            (Packet::List(_), Packet::Integer(v2)) => self.compare(&Packet::List(vec![Packet::Integer(*v2)])),
+            (Packet::List(v1), Packet::List(v2)) => {
+                let ordered = std::iter::zip(v1.iter(), v2.iter())
+                    .fold(None, |acc, (a,b)| 
+                        match acc {
+                            Some(_) => acc,
+                            None => a.compare(b)
+                        }
+                    );
+                if v1.len() < v2.len() && ordered.is_none() {
+                    Some(true)
+                } else if v1.len() > v2.len() && ordered.is_none() {
+                    Some(false)
+                } else {
+                    ordered
+                }
+            },
+            _ => panic!()
+        }
+    }
+}
 
 pub struct Day13 {
     input: Input,
@@ -16,12 +61,21 @@ pub struct Day13 {
 impl Day13 {
     pub fn parse(input: &str) -> Day13 {
         Day13 {
-            input: input.to_string(),
+            input: input.split("\n\n").map(|ll| {
+                let (p1, p2) = ll.split_once('\n').unwrap();
+                (Packet::parse(p1).unwrap().1, Packet::parse(p2).unwrap().1)
+            }).collect(),
         }
     }
 
     pub fn part1(&self) -> usize {
-        0
+        self.input
+            .iter()
+            .enumerate()
+            .inspect(|(i,(a,b))| println!("{} {}",i,a.compare(b).unwrap()))
+            .filter(|(_i,(a,b))| a.compare(b).unwrap())
+            .map(|(i,_)| i+1)
+            .sum()
     }
 
     pub fn part2(&self) -> usize {
@@ -29,44 +83,49 @@ impl Day13 {
     }
 }
 
-#[derive(PartialEq, Debug)]
-enum Packet {
-    Integer(usize),
-    List(Vec<Packet>),
-}
-
-fn packet(input: &str) -> IResult<&str, Packet> {
-    Ok(alt((
-        delimited(
-            char('['),
-            map(separated_list0(char(','), packet), |p| Packet::List(p)),
-            char(']'),
-        ),
-        map(digit1, |out: &str| Packet::Integer(out.parse().unwrap())),
-    ))(input)?)
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const SAMPLE: &'static str = "";
+    const SAMPLE: &'static str = "[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]";
 
     #[test]
     fn parser() {
         let t = "[123,456]";
         assert_eq!(
-            packet(t),
+            Packet::parse(t),
             Ok((
                 "",
                 Packet::List(vec![Packet::Integer(123), Packet::Integer(456)])
             ))
         );
         let t = "[]";
-        assert_eq!(packet(t), Ok(("", Packet::List(vec![]))));
+        assert_eq!(Packet::parse(t), Ok(("", Packet::List(vec![]))));
         let t = "[1,[1,2,[]]]";
         assert_eq!(
-            packet(t),
+            Packet::parse(t),
             Ok((
                 "",
                 Packet::List(vec![
@@ -84,6 +143,7 @@ mod tests {
     #[test]
     fn solve() {
         let parse = Day13::parse(SAMPLE);
+        println!("{:?}", parse.input);
         assert_eq!(parse.part1(), 0);
         assert_eq!(parse.part2(), 0);
     }
